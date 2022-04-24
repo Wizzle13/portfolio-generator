9b5b79ff6d96925c0fc273fa4f683622061a7da0
CBurton <burton.chris13@gmail.com>
2022-04-24 13:15:39 -0500

    added inquirer function

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
+// const pageHTML = generatePage(name,github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+//   console.log('portifolio complete! Check ou index.html to see the output!');
+// });
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
-  console.log('portifolio complete! Check ou index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(anwsers => console.log(anwsers));
